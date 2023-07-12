721e0ed1d94c5a1f871f65fa67e8edda99886e4a
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-07-12 15:30:57 +0300

    Added the logout functionality to the user app

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from user.views import CreateUserView, CreateTokenView, ManageUserView
+from user.views import (
+    CreateUserView,
+    ManageUserView,
+    LogoutView)
+
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("me/", ManageUserView.as_view(), name="manage"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import TokenError
 
 from user.serializers import UserSerializer
 
@@ -10,13 +14,28 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class CreateTokenView(ObtainAuthToken):
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(
+                {"success": "Logout successful."}, status=status.HTTP_205_RESET_CONTENT
+            )
+
+        except TokenError:
+            return Response(
+                {"error": "Invalid refresh token."}, status=status.HTTP_400_BAD_REQUEST
+            )
